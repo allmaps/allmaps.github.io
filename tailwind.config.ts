aab78c9ebfc25a2ded93648a9545292cfb4b8360
Bert Spaan <bert.spaan@gmail.com>
2023-09-20 11:04:05 +0200

    added .md to tailwind content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    './src/**/*.{html,js,svelte,ts}',
+    './src/**/*.{html,js,svelte,ts,md}',
     './node_modules/@allmaps/ui/dist/components/**/*.{html,js,svelte,ts}'
   ],
   theme: {
